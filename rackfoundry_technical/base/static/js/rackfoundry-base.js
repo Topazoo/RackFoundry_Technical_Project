17d8daef1577eeaf5c441046f948e9667533a191
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-11-02 16:26:28 -0700

    Created link to character wikis but removed due to incorrect links in API data

--- rackfoundry_technical/base/static/js/rackfoundry-base.js
+++ rackfoundry_technical/base/static/js/rackfoundry-base.js
@@ -1,5 +1,34 @@
-function make_list(json_items) {
-    /* Build an HTML list from JSON items */
+function create_link(link_text, href) {
+    /* Create an <a> tag with a link
+       @ link_text - The link text
+       @ href - The URL to link to */
+
+       var att = document.createAttribute("href");
+       att.value = href;
+
+       var a = document.createElement('a');
+       a.setAttributeNode(att);
+       var text = document.createTextNode(link_text);
+       a.appendChild(text);
+
+       return a;
+}
+
+function get_wiki(json_urls) {
+    /* Find a wiki page link if in JSON
+       @json_urls - The section of the JSON containing URLS */
+
+        for (var i = 0; i < json_urls.length; i++)
+            if(json_urls[i]["type"] === "wiki")
+                return json_urls[i]["url"]
+
+        return null;
+}
+
+function make_list(json_items, empty_placeholder) {
+    /* Build an HTML list from JSON items
+     @ json_items - A list of items from the JSON
+     @ empty_placeholder - The message to insert if no items */
 
     var list = document.createElement('ul');
 
@@ -10,7 +39,7 @@ function make_list(json_items) {
         var item = document.createElement('li');
 
         /* Insert text */
-        item.appendChild(document.createTextNode("None"));
+        item.appendChild(document.createTextNode(empty_placeholder));
 
         /* Add to list */
         list.appendChild(item);
@@ -34,7 +63,8 @@ function make_list(json_items) {
 }
 
 function build_modal(char_json) {
-    /* Insert content into modal */
+    /* Insert content into modal
+     @ char_json - A chunk of the search result JSON containing only information about the clicked character */
 
     /* Insert the full sized image in the modal */
     var att = document.createAttribute("src");
@@ -42,7 +72,18 @@ function build_modal(char_json) {
     document.getElementById("char-image").setAttributeNode(att);
 
     /* Insert the character name */
-    document.getElementById("char-name").innerHTML = char_json.name;
+    var name = document.createElement('p');
+    name.innerHTML = char_json.name;
+    document.getElementById("char-name").replaceChild(name, document.getElementById("char-name").childNodes[0]);
+
+    /* var wiki = get_wiki(char_json["urls"]);
+    if(wiki) {
+        var a = create_link(char_json.name, wiki);
+    }
+    else {
+        var a = document.createElement('p');
+        a.innerHTML = char_json.name;
+    } */ //TODO - Remove bc of irrelevant/incorrect wiki links
 
     /* Insert the character description */
     if (char_json.description.trim().length === 0 )
@@ -51,12 +92,19 @@ function build_modal(char_json) {
         document.getElementById("char-desc").innerHTML = "Description: " + char_json.description; //TODO - replace unicode
 
     /* Insert a list of comics */
-    var comic_list = make_list(char_json["comics"]["items"]);
+    var comic_list = make_list(char_json["comics"]["items"], "No comic appearances."); //TODO - Cap at 11 and allow for pagination
     var dom_comic_list = document.getElementById("char-comics");
     dom_comic_list.replaceChild(comic_list, dom_comic_list.childNodes[0]);
 
-    //TODO - Build series list
-    //TODO - Build stories list
+    /* Insert a list of series */
+    var series_list = make_list(char_json["series"]["items"], "No series appearances."); //TODO - Cap at 11 and allow for pagination
+    var dom_series_list = document.getElementById("char-series");
+    dom_series_list.replaceChild(series_list, dom_series_list.childNodes[0]);
+
+    /* Insert a list of stories */
+    var stories_list = make_list(char_json["stories"]["items"], "No story appearances."); //TODO - Cap at 11 and allow for pagination
+    var dom_stories_list = document.getElementById("char-stories");
+    dom_stories_list.replaceChild(stories_list, dom_stories_list.childNodes[0]);
 }
 
 function char_click(char_json) {
