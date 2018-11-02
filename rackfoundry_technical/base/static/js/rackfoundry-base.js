077db307fef1ea056d3787a07db2c22cbe86d081
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-11-02 03:45:18 -0700

    Image, name and description passed to modal by JS on thumbnail click

--- rackfoundry_technical/base/static/js/rackfoundry-base.js
+++ rackfoundry_technical/base/static/js/rackfoundry-base.js
@@ -2,14 +2,20 @@ function char_click(char_json) {
     if ($(".char-modal-bg").is(":hidden"))
     {
         $(".char-modal-bg").css("display", "block");
+
+        var att = document.createAttribute("src");
+        att.value = char_json.thumbnail.path + '.' + char_json.thumbnail.extension;
+
+        document.getElementById("char-image").setAttributeNode(att);
+        document.getElementById("char-name").innerHTML = char_json.name;
+        document.getElementById("char-desc").innerHTML = "Description: " + unescape(char_json.description);
+        window.alert(char_json.description)
     }
     else
     {
         $(".char-modal-bg").hide();
     }
 
-    var parsed_json = JSON.stringify(char_json);
-    window.alert(parsed_json);
     return false;
 }
 
