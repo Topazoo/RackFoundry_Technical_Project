3de84ff24ddc337011333002b42f16e342be139f
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-11-01 20:26:15 -0700

    Successfully queries Marvel characters API

---
+++ rackfoundry_technical/base/marvel_functions.py
@@ -0,0 +1,58 @@
+import hashlib
+import datetime
+import os
+import sys
+
+def read_ini():
+    ''' Reads the configuration file for Marvel API data'''
+
+    ini_dict = {}
+
+    # Parse server_config.ini and record values
+    if os.path.isfile('server_config.ini'):
+        with open('server_config.ini') as config:
+            for line in config:
+                index = line.find("=")
+
+                if index == -1:
+                    print "server_config.ini error - Invalid Syntax"
+                    sys.exit(1)
+
+                ini_dict[line[:index]] = line[index+1:].strip()
+
+            config.close()
+
+        # Check errors
+        if "marvel_public" not in ini_dict.keys():
+            print "server_config.ini error - No Marvel public key specified"
+            sys.exit(1)
+        if "marvel_private" not in ini_dict.keys():
+            print "server_config.ini error - No Marvel private key specified"
+            sys.exit(1)
+
+    return ini_dict
+
+def create_query_string(query):
+    ''' Create a Marvel API character query string
+        @query - The string to search for characters with '''
+
+    # Get Marvel API keys
+    API_keys = read_ini()
+
+    # URL to be formatted
+    url_template = "http://gateway.marvel.com/v1/public/characters?nameStartsWith={}&ts={}&apikey={}&hash={}"
+
+    # Create timestamp
+    timestamp = datetime.datetime.now().strftime("%x-%X")
+
+    # Hash timestamp, private key and public key
+    md5_hash = hashlib.md5(timestamp + API_keys["marvel_private"] + API_keys["marvel_public"]).hexdigest()
+
+    # Build query string using template
+    full_query = url_template.format(query, timestamp, API_keys["marvel_public"], md5_hash)
+
+    return full_query
+
+
+
+
--- rackfoundry_technical/base/views.py
+++ rackfoundry_technical/base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from marvel_functions import create_query_string
+import urllib2
+import json
 
 def home(request):
     ''' Render the main homepage '''
@@ -6,7 +9,21 @@ def home(request):
 
 def marvel_home(request):
     ''' Render the marvel homepage '''
-    return render(request, 'base/marvel_home.html', {})
+
+    result_json = {}
+    query_string = create_query_string("cable") #TODO Get from form
+
+    try:
+        query_response = urllib2.urlopen(query_string)
+        result_json = json.load(query_response)
+
+    except urllib2.HTTPError as error:
+        error_message = error.read()
+        print error_message
+
+    print result_json
+
+    return render(request, 'base/marvel_home.html', {}) #TODO - Render error on page if not successful
 
 def tickets_home(request):
     ''' Render the tickets homepage '''
