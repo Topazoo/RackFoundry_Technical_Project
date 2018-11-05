e2f5020b99f9f7045456f46e68c6cf1ac85a72bb
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-11-04 20:20:33 -0800

    Fixing unicode hosts bug

--- rackfoundry_technical/rackfoundry_technical/settings.py
+++ rackfoundry_technical/rackfoundry_technical/settings.py
@@ -60,7 +60,7 @@ ini = read_ini()
 if len(ini) == 0:
     ini['secret_key'] = os.environ.get('KEY')
     ini['debug'] = os.environ.get('DEBUG')
-    ini['hosts'] = os.environ.get('HOSTS')
+    ini['hosts'] = unicode(os.environ.get('HOSTS'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
